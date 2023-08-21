f198554edbd7e69029134f74268238229e8029dc
Cam <37002723+CallMeCam@users.noreply.github.com>
2023-08-20 20:34:28 -0400

    created calculator on page

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// operator functions
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+const operate = function (operator, a, b) {
+    return operator(a, b);
+}
+
+// button functions
+
