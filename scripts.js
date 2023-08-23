61ff722a2321c833ecb98627b1213580895d91af
Cam <37002723+CallMeCam@users.noreply.github.com>
2023-08-23 17:37:34 -0400

    added stringing functionality
    added ability to string operations togeter, solving for each pair of numbers on operator button click

--- scripts.js
+++ scripts.js
@@ -15,41 +15,53 @@ const divide = function (a, b) {
     return a / b;
 }
 
-const operate = function (operator, a, b) {
-    return operator(a, b);
-}
-
-// create an empty object for saving values and operators when receiveingh input from user
+// create empty vars for storing numbers, operator, and solution
 let firstNumber = '';
 let secNumber = '';
-let operator = '';
+let operator = null;
+let currentSolution = 0;
 
 // button functions
 const valueBtns = document.querySelectorAll('[valueBtn]');
 const operatorBtns = document.querySelectorAll('[operatorBtn]');
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('delete');
+const equalsBtn = document.getElementById('equals');
 const displayValue = 0;
 
 // add button event listeners
 clearBtn.addEventListener('click', () => {clearDisplay()});
 
 valueBtns.forEach((button) => button.addEventListener('click', () => {
-    displayUpdate(button.textContent);
-
+    if (operator === null) {
+        firstNumber += button.textContent;
+        displayUpdate(firstNumber);
+    } else {
+        secNumber += button.textContent;
+        displayUpdate(secNumber);
+    }
 }))
 
 operatorBtns.forEach((button) => button.addEventListener('click', () => {
-    displayUpdate(button.textContent);
+    // add function so that if operator is already set to a value, it solves and shows result
+        // after it solves, all values are reset except for display value
+    if (operator === null) {
+        operator = button.textContent;
+        displayUpdate(button.textContent);
+    } else {
+        currentSolution = calcSolution(operator, firstNumber, secNumber);
+        displayUpdate(currentSolution);
+        firstNumber = currentSolution;
+        secNumber = '';
+        operator = button.textContent;
+    }
 }))
 
-const numberUpdate = function (numVariable, value) {
-    numVariable = value;
-}
-
-const operatorHandler = function (a, b, operator) {
-
-}
+equalsBtn.addEventListener('click', () => {
+    currentSolution = calcSolution(operator, firstNumber, secNumber);
+    displayUpdate(currentSolution);
+    resetVals();
+}) 
 
 // change the display on button click
 const display = document.getElementById('screen-text');
@@ -58,15 +70,24 @@ const displayUpdate = function (buttonContent) {
     display.textContent = buttonContent;
 }
 
+const resetVals = function () {
+    firstNumber = '';
+    secNumber = '';
+    operator = null;
+    currentSolution = 0;
+}
+
 const clearDisplay = function () {
     displayUpdate(0);
     firstNumber = '';
     secNumber = '';
-    operator = '';
+    operator = null;
 }
 
 // calculate solution based on operator
 const calcSolution = function (operator, a, b) {
+    a = Number (a);
+    b = Number (b);
     switch (operator) {
         case '+': return add(a, b);
         case '-': return subtract(a, b);
